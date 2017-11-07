926969f55ab1f804187cf07c2f0fb642b9cf0df9
meRahulSingh <mr.rahulsingh@hotmail.com>
2017-11-07 10:09:54 +0100

    all

---
+++ Lambda/Name.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda
+{
+    public  class Name
+    {
+        
+        private string sName { get; set; }
+               
+        public Name(string studentname)
+        {
+
+            sName = studentname;
+
+            List<string> studentnamelist     = new List<string>();
+            studentnamelist .Add("Rahul");
+            studentnamelist.Add("Taleb");
+            studentnamelist.Add("Faizal");
+            studentnamelist.Add("John");
+
+
+
+        
+            foreach (var item in studentnamelist.Where(x => x.StartsWith(sName)))
+            {
+                Console.WriteLine("The student name is  :"+item);
+                Console.ReadLine();
+            }
+
+
+        }
+    }
+}
---
+++ Lambda/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda
+{
+    class Program
+    {
+        public static string UserWants { get; private set; }
+
+        static void Main(string[] args)
+        {
+            //Name clsInst = new Name(studentname);
+            //// clsInst.studentName();
+            //UserNameWanted(UserWants);
+            UserNameWanted(UserWants);
+            
+          }
+
+        public static string UserNameWanted(string UserWants)
+        {
+            Console.WriteLine("Enter the first letter you want from the student list:");
+            UserWants = Console.ReadLine();
+            Name UserInput = new Name(UserWants);
+            return UserWants;
+        }
+    }
+}
